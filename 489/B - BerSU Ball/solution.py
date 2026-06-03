12a632314beefbe3b8187dc944f6f254054c3d32
Sneha Chandravanshi <161471021+SnehaChandravanshi@users.noreply.github.com>
2026-06-03 21:14:22 +0530

    Add BerSU Ball [B] from Codeforces

---
+++ 489/B - BerSU Ball/solution.py
@@ -0,0 +1,19 @@
+n = int(input())
+boys = sorted(list(map(int, input().split())))
+m = int(input())
+girls = sorted(list(map(int, input().split())))
+ 
+pairs = 0
+i = j = 0
+ 
+while i < n and j < m:
+    if abs(boys[i] - girls[j]) <= 1:
+        pairs += 1
+        i += 1
+        j += 1
+    elif boys[i] < girls[j]:
+        i += 1
+    else:
+        j += 1
+ 
+print(pairs)
